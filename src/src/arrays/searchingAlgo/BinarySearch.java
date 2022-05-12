9f88182fdf40621957af8de05c75177b3092b88c
Munaga Nithin Kumar <munaganithin.mnk@gmail.com>
2022-05-12 16:56:02 +0530

    Minimum index element in sorted rotated array

--- src/src/arrays/searchingAlgo/BinarySearch.java
+++ src/src/arrays/searchingAlgo/BinarySearch.java
@@ -36,4 +36,28 @@ public class BinarySearch {
         }
         return modifiedBinarySearch(arr,ele,leftIndex,mid-1);
     }
+
+    private int func(int[] arr, int start, int end) {
+        if(arr[start]<=arr[end]) {
+            return start;
+        }
+        if(end==start+1) {
+            if(arr[start]<arr[end]) {
+                return start;
+            }
+            return end;
+        }
+        int mid = start + (end-start)/2;
+        if(arr[mid]<arr[mid+1] && arr[mid]<arr[mid-1]) {
+            return mid;
+        }
+        if(arr[start]<=arr[mid]) {
+            return func(arr,mid+1,end);
+        }
+        return func(arr,start,mid-1);
+    }
+
+    public int minElementSortedRotatedArray(int[] arr) {
+        return func(arr,0,arr.length-1);
+    }
 }
