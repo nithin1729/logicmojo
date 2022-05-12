ad4decee85aab650a15535e4af80151d7636520d
Munaga Nithin Kumar <munaganithin.mnk@gmail.com>
2022-05-12 15:58:11 +0530

    Search in sorted rotated array

--- src/src/MainMethod.java
+++ src/src/MainMethod.java
@@ -11,12 +11,15 @@ public class MainMethod {
 
         BinarySearch binarySearchObj = new BinarySearch();
         int[] sortedArr = {1,2,3,4,5,6,7,8};
-        System.out.println(binarySearchObj.binarySearch(sortedArr, 7, 0, sortedArr.length-1));
+        System.out.println(binarySearchObj.modifiedBinarySearch(sortedArr, 7, 0, sortedArr.length-1));
+        int[] sortedRotatedArr = {4,5,6,7,0,1,2};
+        System.out.println(binarySearchObj.modifiedBinarySearch(sortedRotatedArr,0,0,sortedRotatedArr.length-1));
+        System.out.println(binarySearchObj.modifiedBinarySearch(sortedRotatedArr,3,0,sortedRotatedArr.length-1));
 
         MergeSort mergeSortObj = new MergeSort();
         mergeSortObj.mergeSort(unsortedArr, 0, unsortedArr.length-1);
-        for(int i=0; i<unsortedArr.length; i++) {
-            System.out.print(unsortedArr[i] + " ");
+        for(int ele: unsortedArr) {
+            System.out.print(ele + " ");
         }
         System.out.println();
 
--- src/src/arrays/searchingAlgo/BinarySearch.java
+++ src/src/arrays/searchingAlgo/BinarySearch.java
@@ -14,4 +14,26 @@ public class BinarySearch {
         }
         return binarySearch(arr, ele, mid+1, lastIndex);
     }
+
+    public int modifiedBinarySearch(int[] arr, int ele, int leftIndex, int rightIndex) {
+        if(leftIndex>rightIndex) {
+            return -1;
+        }
+        int mid = leftIndex + (rightIndex-leftIndex)/2;
+        if(arr[mid]==ele) {
+            return mid;
+        }
+        if(arr[leftIndex]<=arr[mid]) {
+            //left is sorted
+            if(arr[leftIndex]<=ele && ele<=arr[mid]) {
+                return modifiedBinarySearch(arr,ele,leftIndex,mid-1);
+            }
+            return modifiedBinarySearch(arr,ele,mid+1,rightIndex);
+        }
+        //right is sorted
+        if(arr[mid]<=ele && ele<=arr[rightIndex]) {
+            return modifiedBinarySearch(arr,ele,mid+1,rightIndex);
+        }
+        return modifiedBinarySearch(arr,ele,leftIndex,mid-1);
+    }
 }
