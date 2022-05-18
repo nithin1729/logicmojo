6fc5369b7b8719fdf65412dc6759a88b3177d192
Munaga Nithin Kumar <munaganithin.mnk@gmail.com>
2022-05-18 15:48:13 +0530

    Maximum Profit with unlimited buy and sell in stocks and at max only one open position

---
+++ src/src/arrays/misc/StockBuySellMaximumProfit.java
@@ -0,0 +1,11 @@
+package arrays.misc;
+
+public class StockBuySellMaximumProfit {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for(int i=1; i<prices.length; i++) {
+            profit += (prices[i]>prices[i-1])? prices[i]-prices[i-1]:0;
+        }
+        return profit;
+    }
+}
