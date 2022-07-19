c8dfd47f1b15e5faada2bea848036f2019f8cbfa
Munaga Nithin Kumar <munaganithin.mnk@gmail.com>
2022-07-19 16:02:41 +0530

    Converting number to word where number limit is 9 digit

---
+++ src/src/hashing/NumberToWord.java
@@ -0,0 +1,45 @@
+package hashing;
+
+public class NumberToWord {
+    String[] ones = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    String[] tens = {"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+    String convert(long numm) {
+        int num = (int)numm;
+        if(num<20) {
+            return ones[num];
+        }
+        if(num%10==0) return tens[num/10];
+        return tens[num/10] + " " + ones[num%10];
+    }
+    String convertToWords(long n) {
+        // code here
+        if(n==0) return "zero";
+        long last2 = n%100;
+        n = n/100;
+        long hundred = n%10;
+        n = n/10;
+        long thousands = n%100;
+        n = n/100;
+        long lakhs = n%100;
+        n = n/100;
+        long crores = n;
+        String ans = "";
+        if(crores>0) {
+            ans = ans + convert(crores) + " crore ";
+        }
+        if(lakhs>0) {
+            ans = ans + convert(lakhs) + " lakh ";
+        }
+        if(thousands>0) {
+            ans = ans + convert(thousands) + " thousand ";
+        }
+        if(hundred>0) {
+            ans = ans + convert(hundred) + " hundred ";
+        }
+        if(last2>0) {
+            if(ans.length() > 0) ans = ans + "and ";
+            ans = ans + convert(last2);
+        }
+        return ans;
+    }
+}
