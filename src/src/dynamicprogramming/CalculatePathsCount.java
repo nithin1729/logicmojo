363ef55333f4c974167688508b668e69b9ea1310
Munaga Nithin Kumar <munaganithin.mnk@gmail.com>
2022-09-01 01:18:28 +0530

    Calculation of number of paths from top left cell to bottom right cell in a 2D matrix where we can travel in two directions i.e right or down

---
+++ src/src/dynamicprogramming/CalculatePathsCount.java
@@ -0,0 +1,19 @@
+package dynamicprogramming;
+
+public class CalculatePathsCount {
+    long numberOfPaths(int m, int n)
+    {
+        // code here
+        long[][] dp = new long[m][n];
+        for(int i=0; i<m; i++) dp[i][0] = 1;
+        for(int j=0; j<n; j++) dp[0][j] = 1;
+
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                dp[i][j] %= 1000000007;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
